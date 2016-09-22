5e3deb5152e78a7eb01a717500a6b76190eb50b1
Erik van Egmond <erikvane@gmail.com>
2016-09-22 16:37:58 +0200

    add individual constructor

--- src/Individual.java
+++ src/Individual.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 /**
  * Created by erikv on 14-9-2016.
  */
 public class Individual {
+
+    private double[] genome;
+
+    public Individual(int genomeSize){
+        this.genome = new double[genomeSize];
+
+        Random rand = new Random();
+        for(int i = 0; i<genomeSize; i++){
+            this.genome[i] = rand.nextInt(100)-50;
+        }
+        System.out.println(this.genome);
+    }
 }
