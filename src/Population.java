9763f293256a7346b7c4dbdf0a6130b8d513167c
Erik van Egmond <erikvane@gmail.com>
2016-09-22 16:39:13 +0200

    make individuals

--- src/Population.java
+++ src/Population.java
@@ -5,6 +5,7 @@ class Population {
         this.population = new Individual[populationSize];
 
         for(int individuCounter = 0; individuCounter < populationSize; individuCounter++){
+            this.population[individuCounter] = new Individual(10);
             System.out.println(individuCounter);
         }
     }
