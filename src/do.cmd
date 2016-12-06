b7fa96eee2ea0b24d1d997ff8cba61b18ed0be67
erikvanegmond <Bailpzt06>
2016-12-06 20:48:56 +0100

    Remove .class files after running to keep the folder clean

--- src/do.cmd
+++ src/do.cmd
@@ -1,4 +1,5 @@
 javac -cp contest.jar *.java
 jar cmf MainClass.txt submission.jar *.class
 del player17.class
-java -jar testrun.jar -submission=player17 -evaluation=SphereEvaluation -seed=1
+java -jar testrun.jar -submission=player17 -evaluation=DeceptiveEvaluation -seed=1
+del *.class
